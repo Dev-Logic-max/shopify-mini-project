0d19fc5b63950fa0116590b1a1677e4687e98f82
Dev-Logic-max <ar3991492@gmail.com>
2025-07-16 16:14:07 +0500

    Updated env

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  env: {
+    SHOPIFY_STORE_DOMAIN: process.env.SHOPIFY_STORE_DOMAIN,
+    SHOPIFY_ACCESS_TOKEN: process.env.SHOPIFY_ACCESS_TOKEN,
+    SHOPIFY_WEBHOOK_SECRET: process.env.SHOPIFY_WEBHOOK_SECRET,
+  },
 };
 
 export default nextConfig;
